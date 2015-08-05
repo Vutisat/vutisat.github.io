808c1437f5b44826b309a44d75e38cfa9197acc5
Vutisat <vutisat@miamioh.edu>
2015-08-05 08:53:52 -0700

    Added animations to logo, fixed php mailing

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
 <?php 
+header('Location: index.html');
 $name = $_POST['name'];
 $email = $_POST['email'];
-$message = $_POST['msgtext'];
+$message = "Name: " . $name . "\r\n" . "Reply-To:" . $email . "\r\n" .$_POST['msgtext'];
 
-$headers = "From: pob@pobv.me" . "\r\n" . "Reply-To: $email" . "\r\n";
+$headers = "From: pob@pobv.me" . "\r\n";
 
 $to = "Vutisat@miamioh.edu";
 $subject = "New message from the website!";
 
-
 $a = mail( $to, $subject, $message, $headers );
-/*if ($a) 
-{
+if ($a) {
    echo("Message was sent sucessfully! I look forward to reading it!");
-} else {
-   print("Message wasn't sent, please check that you have changed emails in the bottom");}
-
-mail ($to, $subject, $message, $headers);
-echo "Your messge has been sent!";
-*/
-?>
+   exit;
+}else {
+   echo("Message wasn't sent, please check that you have changed emails in the bottom");
+    exit;
+}
